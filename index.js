ea911d54844dab8b5e7bf8b2e978611ce75540c4
Will Kerry <mail@example.com>
2022-04-28 22:59:18 +0100

    Didn't need that commented out code

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-// const app = require("express")();
-// const http = require("http").Server(app);
-// const io = require("socket.io")(http);
-// const port = process.env.PORT || 3001;
-
 const { Server } = require("socket.io");
 
 const io = new Server({
